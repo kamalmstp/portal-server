85647279f85fc0c568f81501ced099e63ed3e8e2
Mustapa Ahmad Kamal <mstpahmdkml@gmail.com>
2019-06-23 19:33:42 +0800

    upload

---
+++ application/views/backend/footer.php
@@ -0,0 +1,7 @@
+<!-- Footer -->
+<footer class="main">
+	&copy; 2019 
+	<a href="http://almazayaislamicschool.sch.id"
+    	target="_blank">SMA Al-Mazaya</a>
+	| Almazaya, Integrated School Management System
+</footer>
